8423edf13e54b53b8fcd92737198773083b91e5c
Ross Smith <rossmith@2379591005.vpn.umich.net>
2019-09-09 13:34:03 -0400

    KPMP-1255 cleaning out .gradle/IDE files; adding stub method to StateController for long polling

--- src/main/java/org/kpmp/stateManager/StateController.java
+++ src/main/java/org/kpmp/stateManager/StateController.java
@@ -1,6 +1,8 @@
 package org.kpmp.stateManager;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.async.DeferredResult;
 
 @Controller
 public class StateController {
@@ -44,4 +47,36 @@ public class StateController {
 		return stateService.getAllCurrentStates();
 	}
 
+//	@RequestMapping(value = "/v1/events/state", method = RequestMethod.GET)
+//	public @ResponseBody List<State> getStateEvents(HttpServletRequest request) {
+//		log.info("URI: " + request.getRequestURI() + " | MSG: Retrieving most recent state for all packages");
+//		return stateService.getAllCurrentStates();
+//	}
+
+	@RequestMapping(value = "/v1/events/state/{afterTime}", method = RequestMethod.GET)
+	public @ResponseBody DeferredResult<List<State>> getStateEvents(@PathVariable("afterTime") String afterTime, HttpServletRequest request) {
+		log.info("URI: " + request.getRequestURI() + " | MSG: Long poll for events after " + afterTime);
+
+		Long timeOutInMilliSec = 100000L;
+		String timeOutResp = "{\"msg\": \"time out\"}";
+		DeferredResult<List<State>> deferredResult = new DeferredResult<>(timeOutInMilliSec,timeOutResp);
+		CompletableFuture.runAsync(()->{
+			try {
+				List<State> result = stateService.getAllCurrentStates();
+
+				while (result.size() == 0) {
+					TimeUnit.SECONDS.sleep(10);
+					result = stateService.getAllCurrentStates();
+				}
+
+				log.info("URI: " + request.getRequestURI() + " | MSG: Long poll returning " + result.size() + " records");
+
+				//set result after completing task to return response to client
+				deferredResult.setResult(result);
+			}catch (Exception ex){
+			}
+		});
+
+		return deferredResult;
+	}
 }
