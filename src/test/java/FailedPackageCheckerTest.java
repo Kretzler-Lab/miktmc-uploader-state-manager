1a68519e7a1509d5e0dc877ae4a5a9cf0f6ba0e7
Ross Smith <rossmith@Rosss-MacBook-Pro.local>
2019-12-06 14:45:45 -0500

    KPMP-1507 largeFilesChecked fields and tests passing

--- src/test/java/FailedPackageCheckerTest.java
+++ src/test/java/FailedPackageCheckerTest.java
@@ -2,12 +2,15 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.kpmp.FailedPackageChecker;
+import org.kpmp.stateManager.State;
 import org.kpmp.stateManager.StateService;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.web.client.RestTemplate;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class FailedPackageCheckerTest {
 
@@ -33,4 +36,18 @@ public class FailedPackageCheckerTest {
         assertEquals(42, packageChecker.getTimeSinceLastModified(System.currentTimeMillis() - 42));
     }
 
+    @Test
+    public void testPackageDidFail() throws Exception {
+        State state = new State();
+        state.setLargeFilesChecked(false);
+
+        long timeSinceLastModified = System.currentTimeMillis() - 29 * 60 * 1000;
+        assertFalse(packageChecker.packageDidFail(state, timeSinceLastModified));
+
+        timeSinceLastModified = System.currentTimeMillis() - 31 * 60 * 1000;
+        assertTrue(packageChecker.packageDidFail(state, timeSinceLastModified));
+
+        state.setLargeFilesChecked(true);
+        assertFalse(packageChecker.packageDidFail(state, timeSinceLastModified));
+    }
 }
